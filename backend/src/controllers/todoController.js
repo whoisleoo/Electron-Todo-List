faddedd157b93103a907341cc9ea430f1dcd94a2
whoisleoo <leoaaa96@gmail.com>
2025-08-08 14:18:22 -0300

    Rota put, delete do todo feita

--- backend/src/controllers/todoController.js
+++ backend/src/controllers/todoController.js
@@ -6,6 +6,10 @@ Verificar se a lista pertence ao usuário
 Buscar todos apenas por listId
 */
 
+// =====================================================================
+//                         BUSCAR TODO
+// =====================================================================
+
 export const buscarTodo = async function(req, res){
     const userId = req.user.id
     const { listId } = req.params;
@@ -33,6 +37,169 @@ export const buscarTodo = async function(req, res){
             todos: allTodo,
             total: allTodo.length,
             list: findList.name
+        })
+    }catch(error){
+        res.status(500).json({
+            error: "Erro interno do servidor",
+            message: error.message
+        })
+    }
+}
+
+// =====================================================================
+//                         CRIAR TODO
+// =====================================================================
+
+export const createTodo = async function(req, res){
+    const { text } = req.body
+    const userId = req.user.id
+    const { listId } = req.params;
+    try{
+
+        if(!text){
+            return res.status(404).json({
+                error: "Nome não especificado."
+            })
+        }
+
+        const findList = await prisma.list.findFirst({
+            where: { id: parseInt(listId), userId: userId}
+        })
+        
+        if(!findList){
+            return res.status(404).json({
+                error: "Você não possui permissão para ver essa lista."
+            })
+        }
+
+        const newTodo = await prisma.todo.create({
+            data: {
+                text: text.trim(),
+                listId: parseInt(listId)
+            }
+
+        });
+
+
+        return res.status(201).json({
+            message: "Todo criada com sucesso",
+            todo: newTodo,
+            list: findList.name
+        })
+    }catch(error){
+        res.status(500).json({
+            error: "Erro interno do servidor",
+            message: error.message
+        })
+    }
+}
+
+
+// =====================================================================
+//                         PUT TODO
+// =====================================================================
+
+export const updateTodo = async function(req, res){
+    const { text, completed } = req.body
+    const userId = req.user.id
+    const { listId, todoId } = req.params;
+    try{
+
+       if(!text){
+            return res.status(404).json({
+                error: "Nome não especificado."
+            })
+        }
+
+        if(completed === null || completed === undefined){
+            return res.status(400).json({
+                error: "Status é obrigatório."
+            })
+        }
+
+        const findList = await prisma.list.findFirst({
+            where: { id: parseInt(listId), userId: userId}
+        })
+        
+        if(!findList){
+            return res.status(404).json({
+                error: "Você não possui permissão para ver essa lista."
+            })
+        }
+
+
+        const todoExists = await prisma.todo.findFirst({
+            where: { id: parseInt(todoId), listId: parseInt(listId) }
+        })
+
+        if(!todoExists){
+            return res.status(404).json({
+                error: "Você não possui permissão para ver esse Todo."
+            })
+        }
+
+        const updateTodo = await prisma.todo.update({
+            where: { id: parseInt(todoId)},
+            data: {
+                text: text.trim(),
+                completed: completed,
+            }
+
+        });
+
+
+        return res.status(200).json({
+            message: "Todo atualizado com sucesso",
+            todo: updateTodo,
+            list: findList.name
+        })
+    }catch(error){
+        res.status(500).json({
+            error: "Erro interno do servidor",
+            message: error.message
+        })
+    }
+}
+
+
+// =====================================================================
+//                         DELETE TODO
+// =====================================================================
+
+export const deleteTodo = async function(req, res){
+    const userId = req.user.id
+    const { listId, todoId } = req.params;
+    try{
+
+
+        const findList = await prisma.list.findFirst({
+            where: { id: parseInt(listId), userId: userId}
+        })
+        
+        if(!findList){
+            return res.status(404).json({
+                error: "Você não possui permissão para ver essa lista."
+            })
+        }
+
+
+        const todoExists = await prisma.todo.findFirst({
+            where: { id: parseInt(todoId), listId: listId }
+        })
+
+        if(!todoExists){
+            return res.status(404).json({
+                error: "Você não possui permissão para ver esse Todo."
+            })
+        }
+
+        const deleteTodo = await prisma.todo.delete({
+            where: { id: parseInt(todoId)},
+        });
+
+
+        return res.status(200).json({
+            message: "Todo deletado com sucesso",
         })
     }catch(error){
         res.status(500).json({
